0dcb96a0262bdbbabc2fe2eec21728f56ac99651
zeitun-adan <zeitun.adan@student.moringaschool.com>
2022-06-10 18:43:42 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = "bob";
+const leastFavoriteCustomer = 'newton'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'newton';
+  }
